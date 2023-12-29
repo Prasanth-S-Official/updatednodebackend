d33b036681659ca05c1f8fde3d5917fa840bfff1
Iamneo <techadmin@iamneo.ai>
2023-12-29 10:26:27 +0000

    Auto-save-commit-12/29/2023--3:56:27-PM

--- backendnode/tests/App.test.js
+++ backendnode/tests/App.test.js
@@ -12,27 +12,30 @@ const {
 
   
   describe('User Controller - File System', () => {
-    test('Register a new user', async () => {
+    const testUniqueId = new Date().getTime(); // Generate a unique timestamp for each test run
+    const uniqueEmail = `test${testUniqueId}@example.com`;
+
+    test(`Register a new user with a unique email (${testUniqueId})`, async () => {
+    
       const req = {
         body: {
           name: 'TestUser',
-          email: 'test@example.com',
+          email: uniqueEmail,
           phoneNo: '1234567890',
           password: 'testpassword',
           role: 'user',
-          id: '123',
+          id: testUniqueId,
         },
       };
-  
+    
       const res = {
         json: jest.fn(),
         status: jest.fn(),
       };
-  
+    
       // Run the register_fs function
       await register_fs(req, res);
-  
-
+    
       // Check the response sent by the controller
       expect(res.json).toHaveBeenCalledWith({
         error: false,
@@ -40,9 +43,37 @@ const {
         data: null,
       });
     });
-  
+    
     // Write similar tests for login_fs, getAllUsers_fs, and resetPassword_fs functions
     // Make sure to mock the request and response objects appropriately for each test
-  });
+    test('User successfully logs in with correct email and password', async () => {
+
+
+        const req = {
+          body: {
+            email: uniqueEmail,
+            password: 'testpassword',
+          },
+        };
+      
+        const res = {
+          json: jest.fn(),
+        };
+      
+        await login_fs(req, res);
+      
+        expect(res.json).toHaveBeenCalledWith({
+          error: false,
+          message: 'Login Successfully',
+          role:"user",
+          email: uniqueEmail,
+          name: 'TestUser'
+        });
+      });
+});
+
+
+
+  
 
 
