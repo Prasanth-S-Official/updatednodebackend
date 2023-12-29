096d35aa40c62da66c7d116cba916dca0e99ea68
Iamneo <techadmin@iamneo.ai>
2023-12-29 09:56:27 +0000

    Auto-save-commit-12/29/2023--3:26:27-PM

--- backendnode/tests/App.test.js
+++ backendnode/tests/App.test.js
@@ -1,37 +1,51 @@
+const fs = require('fs');
+const path = require('path');
 const {
-    writeDataToFile,
-    readDataAndPrint,
-    addData,
-    displayData
-  } = require('../Week7Day1&2'); // Replace 'yourFileName' with the actual filename where your code is located
+  register_fs,
+  login_fs,
+  getAllUsers_fs,
+  resetPassword_fs,
+} = require('../controller/users'); // Replace 'userController' with the actual filename where your code is located
+
+
+
+
   
-  // Test the addData function
-  test('Add user data to dataArray', () => {
-    const userData = {
-      name: 'TestUser',
-      email: 'test@example.com',
-      phoneNo: '1234567890',
-      password: 'testpassword',
-      role: 'user',
-    };
+  describe('User Controller - File System', () => {
+    test('Register a new user', async () => {
+      const req = {
+        body: {
+          name: 'TestUser',
+          email: 'test@example.com',
+          phoneNo: '1234567890',
+          password: 'testpassword',
+          role: 'user',
+          id: '123',
+        },
+      };
   
-    addData(userData, (err, addedUserData) => {
-      expect(err).toBeNull();
-      expect(addedUserData).toEqual(userData);
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+      };
+  
+      // Run the register_fs function
+      await register_fs(req, res);
+  
+      // Check if the user is registered and file is created
+      expect(usersData.length).toBe(1);
+      expect(fs.existsSync(dataFilePath)).toBe(true);
+  
+      // Check the response sent by the controller
+      expect(res.json).toHaveBeenCalledWith({
+        error: false,
+        message: 'User Registration Successful',
+        data: null,
+      });
     });
+  
+    // Write similar tests for login_fs, getAllUsers_fs, and resetPassword_fs functions
+    // Make sure to mock the request and response objects appropriately for each test
   });
-  
-  // Test the displayData function
-  test('Display users in the array', () => {
-    // Mock console.log to capture output
-    const mockLog = jest.spyOn(console, 'log').mockImplementation(() => {});
-  
-    displayData();
-  
-    // Check if console.log was called with the expected output
-    expect(mockLog).toHaveBeenCalledWith('Users in the array:');
-    // Add more expectations as needed based on your actual implementation
-  
-    // Restore the original console.log function
-    mockLog.mockRestore();
-  });
+
+
